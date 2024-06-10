f32b6397ebb1d41c941133a7a61f3a55d7d7fa12
fatima shamsieva <diel1989@mail.ru>
2024-06-11 01:30:20 +0500

    modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from "./data";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [cafe, setCafe] = useState(0);
+  const {id, name, description, image} = data[cafe];
+  const [showMore, setShowMore] = useState(false);
+
+
+  const previousCafe = () => {
+    setCafe((cafe => {
+      cafe --;
+      if (cafe < 0) {
+        return data.length - 1;
+      }
+      return cafe;
+    }))
+  }
+
+  const nextCafe = () => {
+    setCafe((cafe => {
+      cafe ++;
+      if (cafe > data.length - 1) {
+        cafe = 0;
+      }
+      return cafe;
+    }))
+
+  }
+
+return(<div>
+
+  <div className="container">
+    <h1>THE MOST BEAUTIFUL CAFES YOU MUST VISIT IN BALI</h1>
+  </div>
+
+  <div className="container">
+    <img src={image} width="500px" alt="cafe"/>
+  </div>
+
+  <div className="container">
+    <h2>{id} - {name}</h2>
+  </div>
+
+  <div className="btn container">
+    <button className="prevNext" onClick={previousCafe}>Previous</button>
+    <button className="prevNext" onClick={nextCafe}>Next</button>
+  </div>
+
+  <div className="container">
+    <h3>{showMore ? description : description.substring(0,220) + "...."}
+    <button className="btn" onClick={() => setShowMore(!showMore)}>{showMore ? "Show less" : "Show more"}</button>
+    </h3>
+  </div>
+
+</div>
+)
 }
 
 export default App;
+ 
---
+++ src/data.js
@@ -0,0 +1,52 @@
+
+export const data = [
+
+    {
+        id: 1,
+        name: "TOUCHÉ",
+        description: "Touché is the term used when someone admits that another person made a good point, and there couldn’t be a more perfect name to describe this beautiful café. You won’t be disappointed while you are immersed in this minimalist cafe with modern Balinese finishing. The café has more Earth tones and wicker furniture to give you the bohemian feel of Bali. However, the sculptures add a modern touch making this a great space to get inspired. Touché is located in the Canggu area of Bali.",
+        image: "https://i0.wp.com/knycxjourneying.com/wp-content/uploads/2023/02/Bali-Cafe-20-Touche.jpg?resize=840%2C630&ssl=1"
+      },
+
+      {
+        id: 2,
+        name: "THE KOROWAI RESTAURANT",
+        description: "The Korowai in Uluwatu is an adults-only boutique hotel with a restaurant, a bar and a garden. The coffee house located on the property is one of the most beautiful cafes in Bali. It is the perfect place to wake up early and experience one of the best sunrise views in Bali. Please be advised that the property is only reachable by stairs and may not be suitable for those with mobility issues.",
+        image: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/326527919.jpg?k=efb0a9cc2550f4f887f346246072010ae23b712eeb22174c76eba486cccd7104&o=&hp=1"
+      },
+  
+      {
+        id: 3,
+        name: "DOPPIO CAFÉ PINK",
+        description: "Doppio Café Pink welcomes guests with its open air and unique pink walls. You can choose where to sit, but keep in mind that there’s so much to see in this small café. There’s a pink-walled alley, pink bicycle and two different spaces to choose from. You can’t go wrong with either side, but the best view is the space directly behind the pink bike. There, people can watch during a busy Bali morning. If you are interested in the best view in the cafe, get there as early as possible because these seats fill up fast.",
+        image: "https://legacy.travelnoire.com/wp-content/uploads/2023/05/IMG_2396-1568x1176.jpg"
+      },
+  
+      {
+        id: 4,
+        name: "LAZY CATS CAFE",
+        description: "While many of the most beautiful cafes in Bali play on pink and floral energy or a bohemian vibe, Lazy Cats Cafe offers a different kind of beauty. It still offers bohemian flair, but it adds in a mix of fashionable and vintage décor. Walking inside ignites all of your senses because it’s an eccentric atmosphere that serves as a café, bar and gallery in Ubud.",
+        image: "https://eatbook.sg/wp-content/uploads/2023/05/lazy-cats-cafe-ambience.jpg"
+      },
+
+      {
+        id: 5,
+        name: "PENNY LANE",
+        description: "No, it is not a dream. Penny Lane is beautiful, and that’s why it’s trendy in Bali. The café is a Boho-chic restaurant offering Balinese dishes, bar bites and signature cocktails in a stunning open-air venue. It is hard to leave this cafe located in Canggu.",
+        image: "https://thesmartlocal.com/wp-content/uploads/2023/05/Penny-Lane-Bali-wide-shot.jpg"
+      },
+
+      {
+        id: 6,
+        name: "REVOLVER ESPRESSO",
+        description: "Revolver Espresso was created in 2012, as Bali’s first specialty coffee shop. Now, there are multiple Revolver Espresso locations throughout Bali, including at the airport and in Seminyak. The space has a pink wall and beautiful flowers to make all your Instagram pictures come to life.",
+        image: "https://img.trvcdn.net/https://s3.ap-southeast-2.amazonaws.com/thebalibible.com/uploads/images/venue/93026b0f9b9968f4801f658992033035.jpg?v=1?imgeng=m_box/w_1418/h_946"
+      },
+
+      {
+        id: 7,
+        name: "LA MONTAGNE COFFEE",
+        description: "La Montagne Cafe is a space for people who find beauty in nature. Located in Eastern Bali, this café is the only place to get a close view of the majestic Agung Mountain and rice fields. Its beauty is unmatched, and the location is a hidden gem because not many people frequent Eastern Bali. This location is about two hours east of the popular Seminyak area. It’s perfect for people who enjoy being in a less popular place.",
+        image: "https://scontent.ftas2-1.fna.fbcdn.net/v/t39.30808-6/289042601_539632054536200_2800001023677158439_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=SqCunxBwpscQ7kNvgGaRAvB&_nc_ht=scontent.ftas2-1.fna&oh=00_AYA1r_706oh5lgYnyjF_1epGf9P_XNOWHssMceU2mrsbVg&oe=666D33EF"
+      },
+]
